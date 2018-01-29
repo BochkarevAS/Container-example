ef18be42ec16d1a658da32462d5133e2aadf7b2c
BochkarevAS <bochkarev-vl@mail.ru>
2018-01-29 19:05:51 +1000

    Initial commit

---
+++ src/Container.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Example;
+
+class Container {
+
+    protected $instances = [];
+
+    protected $bindings = [];
+
+    public function instance($key, $value) {
+        $this->instances[$key] = $value;
+
+        return $this;
+    }
+
+    public function bind($key, $value) {
+        $this->bindings[$key] = $value;
+
+        return $this;
+    }
+
+    public function make($key) {
+
+        if (array_key_exists($key, $this->instances)) {
+            return $this->instances[$key];
+        }
+
+        if (array_key_exists($key, $this->bindings)) {
+            $resolver = $this->bindings[$key];
+            return $this->instances[$key] = $resolver();
+        }
+
+        if ($instance = $this->autoResolve($key)) {
+            return $instance;
+        }
+
+        throw new \Exception('Not found element into container');
+    }
+
+    public function autoResolve($key) {
+
+        if (!class_exists($key)) {
+            return false;
+        }
+
+        $reflectionClass = new \ReflectionClass($key);
+
+        if (!$reflectionClass->isInstantiable()) {
+            return false;
+        }
+
+        if (!$constructor = $reflectionClass->getConstructor()) {
+            return new $key;
+        }
+
+        $params = $constructor->getParameters();
+        $args = [];
+
+        try {
+            foreach ($params as $param) {
+                $paramClass = $param->getClass()->getName();
+                $args[] = $this->make($paramClass);
+            }
+        } catch (\Exception $e) {
+            throw new \Exception('Unable to resolve !!!');
+        }
+
+        return $reflectionClass->newInstanceArgs($args);
+    }
+}
---
+++ tests/ContainerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Example\Container;
+use PHPUnit\Framework\TestCase;
+
+class Ant {}
+class Foo {}
+class Bar { function __construct(Foo $foo) {} }
+class Baz { function __construct(Bar $bar) {} }
+
+class ContainerTest extends TestCase {
+
+    public function testCreateContanier() {
+        $container = new Container();
+        $this->assertInstanceOf('Example\Container', $container);
+    }
+
+    public function testInstanceContainer() {
+        $container = new Container();
+        $instance = new Ant();
+
+        $container->instance(Ant::class, $instance);
+        $this->assertSame($container->make(Ant::class), $instance);
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testExceptionContainer() {
+        $container = new Container();
+        $container->make('FakeClass');
+    }
+
+    public function testSingletonBindingsContainer() {
+        $container = new Container();
+        $ant = function () { return new Ant(); };
+
+        $container->bind(Ant::class, $ant);
+        $this->assertInstanceOf('Ant', $container->make(Ant::class));
+        $ant = $container->make(Ant::class);
+        $this->assertSame($ant, $container->make(Ant::class));
+    }
+
+    public function testResolveClassContainer() {
+        $container = new Container();
+        $ant = $container->autoResolve(Ant::class);
+        $this->assertInstanceOf('Ant', $ant);
+    }
+
+    function testDependenciesContainer() {
+        $container = new Container();
+        $baz = $container->make(Baz::class);
+        $this->assertInstanceOf('Baz', $baz);
+    }
+}
